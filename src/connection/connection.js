b919bf2459b44a91b9516bf7b4a575298cc951e0
WillyRumapea <willyrumapea123jr@gmail.com>
2024-06-10 10:25:00 +0700

    menghubungkan ke cloud sql

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2/promise");
 
+process.env.DB_USER = "root";
+process.env.DB_PASS = "mo-rotieProject";
+process.env.DB_NAME = "mold_database";
+
 const connectDB = async () => {
   const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "mold_database",
+    socketPath: `/cloudsql/mo-rotie:asia-southeast2:mo-rotie-project`,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   });
   console.log("connected to MySQL");
   return connection;
