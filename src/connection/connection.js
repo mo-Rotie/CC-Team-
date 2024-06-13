9546915d7f82d82adcdd659eae5fd652adadf337
WillyRumapea <willyrumapea123jr@gmail.com>
2024-06-13 22:58:51 +0700

    perbarui connection ke cloud sql

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2/promise");
 
 process.env.DB_USER = "root";
-process.env.DB_PASS = "mo-rotieProject";
+process.env.DB_PASS = "12juni2024";
 process.env.DB_NAME = "mold_database";
 
 const connectDB = async () => {
   const connection = await mysql.createConnection({
-    socketPath: `/cloudsql/mo-rotie:asia-southeast2:mo-rotie-project`,
+    socketPath: `/cloudsql/mo-rotie:asia-southeast2:api-article-capstone`,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
